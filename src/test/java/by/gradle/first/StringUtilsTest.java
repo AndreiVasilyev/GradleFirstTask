11315afdcf2c8834547b1db7c16d11a63cdc492b
Andrei Vasilyev <andrei.vasilyev80@gmail.com>
2024-09-01 13:19:01 +0300

    issue#2 corrected static import from * to directly methods

--- src/test/java/by/gradle/first/StringUtilsTest.java
+++ src/test/java/by/gradle/first/StringUtilsTest.java
@@ -2,7 +2,10 @@ package by.gradle.first;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class StringUtilsTest {
 
